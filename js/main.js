f264f0486b0f543271b190c06a1b9d9a6d8b99ce
Marcus Hansson <Marcus.Hansson@axson.local>
2017-08-21 11:46:04 +0200

    Divs are now changing color to black when hovered over

--- js/main.js
+++ js/main.js
@@ -1,34 +1,35 @@
 $(document).ready(function(){
+  // Default etchasketch dim
   matrix_dimension = 16;
 
+  // Get #surface dimensions
   screen_width = $("#surface").width();
   screen_height = $("#surface").height();
 
+  createMatrix(matrix_dimension);
+});
+
+$("#surface").hover(function(){
+  console.log("entered surface")
+  $("div").hover(function(){
+    $(this).css("background", "black");
+  });
+}),function(){console.log("left surface");};
+
+function createMatrix(size){
   // Get the pixelwidth and height and round them down to Integers
-  pix_width = /*Math.floor*/(screen_width / matrix_dimension);
-  pix_height = /*Math.floor*/(screen_height / matrix_dimension);
+  var pix_width = /*Math.floor*/(screen_width / size);
+  var pix_height = /*Math.floor*/(screen_height / size);
 
-  console.log(pix_width);
-  console.log(pix_height);
-
-  // make a foorloop that fills upp with a set of .pix classes
-  // inside of the screen div.
-
+  // Fill the #surface id with pixels
   for(x=0;x < Math.pow(matrix_dimension,2);x++){
     var $pixel = $("<div id=pix"+x+"></div>");
     $("#surface").append($pixel);
     $("#pix"+x).css("width", pix_width);
     $("#pix"+x).css("height" ,pix_height);
   }
+}
 
+function destroyMatrix(){
 
-});
-
-$("#surface>div").hover(function(){
-  $(this).css("background", "black");
-});
-
-//Instead of class use ID
-// move .css width and height inside of the loop
-// then maybe hover works.
-// "<div id=pix"+x+"></div">
+}
