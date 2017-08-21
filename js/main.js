b616efb0e5457a5a30d4057ecc825e0677fda67a
Marcus Hansson <Marcus.Hansson@axson.local>
2017-08-21 14:27:49 +0200

    updated

--- js/main.js
+++ js/main.js
@@ -10,11 +10,13 @@ $(document).ready(function(){
 });
 
 $("#surface").hover(function(){
-  console.log("entered surface")
-  $("div").hover(function(){
+  console.log("entered surface");
+  $("#surface>div").hover(function(){
     $(this).css("background", "black");
   });
-}),function(){console.log("left surface");};
+},function(){
+  console.log("left surface");
+});
 
 function createMatrix(size){
   // Get the pixelwidth and height and round them down to Integers
